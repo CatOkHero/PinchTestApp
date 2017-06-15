a7a77a7c0f454250f97f6e8eae3e56e83de09acc
exoft-user <exoft-user@exoft.net>
2017-06-15 20:59:13 +0300

    initial commit

---
+++ PinchTestApp/TestApp/TestApp/Controls/GridWithGesture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TestApp.Controls
+{
+	public class GridWithGesture : Grid
+	{
+		public event EventHandler<PinchGestureUpdatedEventArgs> PinchAction;
+
+		public void OnPinchAction(Element element, PinchGestureUpdatedEventArgs args)
+		{
+			PinchAction?.Invoke(element, args);
+		}
+	}
+}
---
+++ PinchTestApp/TestApp/TestApp/Effects/TouchEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TestApp.ViewModel
+{
+    public class TouchEffect : RoutingEffect
+    {
+        public event TouchActionEventHandler TouchAction;
+
+	    public event EventHandler<PinchGestureUpdatedEventArgs> PinchAction;
+
+        public TouchEffect() : base("new.new1")
+        {
+        }
+
+        public bool Capture { set; get; }
+
+        public void OnTouchAction(Element element, TouchActionEventArgs args)
+        {
+            TouchAction?.Invoke(element, args);
+        }
+
+	    public void OnPinchAction(Element element, PinchGestureUpdatedEventArgs args)
+	    {
+		    PinchAction?.Invoke(element, args);
+		}
+    }
+}
